0b585080898adec40b6b7710c1678def862578d8
takapon564 <ku.takahito058@gmail.com>
2018-12-22 08:13:53 +0900

    コードの実行

--- type/Example04.java
+++ type/Example04.java
@@ -1,7 +1,7 @@
 public class Example04 {
   public static void main (String[] args) {
-    String badHaiku = "古池屋　蛙飛び込む　水の音"
-    String goodHaiku = badHaiku.replace("古池屋", "古池や")
-    System.out.println(goodHaiku)
+    String badHaiku = "古池屋　蛙飛び込む　水の音";
+    String goodHaiku = badHaiku.replace("古池屋", "古池や");
+    System.out.println(goodHaiku);
   }
 }
